346aaf17aabfc4a3cfa5cf916f41332377c4a693
Martin Kiesel <martin.kiesel@gmail.com>
2016-04-09 15:31:45 +0200

    added typedefs for ads_2 project

---
+++ ads-assignment-3/src/ads_2_typedefs.hpp
@@ -0,0 +1,20 @@
+//
+//  ads_2_typedefs.hpp
+//  ads-assignment-3
+//
+//  Created by Martin Kiesel on 09/04/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#ifndef ads_2_typedefs_h
+#define ads_2_typedefs_h
+
+namespace ads_2
+{
+    namespace type {
+        typedef uint_fast32_t uintf;
+        typedef std::map<std::string, uintf> Data;
+    }
+}
+
+#endif /* ads_2_typedefs_h */
