fee808ae12582380c7281d2bb72a9a73146e33c0
Martin Kiesel <martin.kiesel@gmail.com>
2016-04-10 00:52:00 +0200

    testing main - for obst; Xcode project files updated

--- ads-assignment-3/main.cpp
+++ ads-assignment-3/main.cpp
@@ -8,14 +8,28 @@
 
 #include <iostream>
 
-#include "src/FileParser.hpp"
+
+//#include "src/file_parser.hpp"
+#include "src/optimal_binary_tree/binary_search_tree.hpp"
 
 int main(int argc, const char * argv[]) {
     using namespace ads_2;
-
     using parser::FileParser;
-    FileParser parser("dictionary-small.txt", 10);
+    using binary_tree::BinarySearchTree;
 
+    FileParser parser("dictionary-min.txt", 1000);
+    BinarySearchTree bst(&parser);
+    size_t start = time(NULL);
+    bst.compute();
+    size_t end = time(NULL);
+    std::cout << end - start << std::endl;
+    std::cout << bst.getNodeCount() << std::endl;
 
+    std::string word;
+    while (std::cin >> word)
+    {
+        if (word == "abc") break;
+        std::cout << word << ": " << bst.search(word) << std::endl;
+    }
     return 0;
 }
