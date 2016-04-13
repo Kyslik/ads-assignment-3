fe5fe0353802e1596f8b69fce30aafd0e5d56d6f
Martin Kiesel <martin.kiesel@gmail.com>
2016-04-13 22:09:38 +0200

    typo

--- ads-assignment-3/src/ads_2_typedefs.hpp
+++ ads-assignment-3/src/ads_2_typedefs.hpp
@@ -20,6 +20,7 @@ namespace ads_2
         typedef uint_fast16_t uintf16;
         typedef std::map<std::string, const uintf> Data;
         typedef std::pair<std::string, uintf> DataPair;
+
         typedef std::vector<DataPair> Pairs;
         typedef std::vector<uintf> Dimension;
         typedef std::vector<Dimension> Matrix;
