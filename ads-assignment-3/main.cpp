bb0419914b1081c3e028d1ba255146f37f38787f
Martin Kiesel <martin.kiesel@gmail.com>
2016-04-15 01:09:15 +0200

    testing main with samples

--- ads-assignment-3/main.cpp
+++ ads-assignment-3/main.cpp
@@ -7,32 +7,50 @@
 //
 
 #include <iostream>
+#include <ctime>
 
-
-//#include "src/file_parser.hpp"
+#include "src/file_parser.hpp"
 #include "src/optimal_binary_tree/binary_search_tree.hpp"
+#include "src/hash_table/hash_table.hpp"
 
 int main(int argc, const char * argv[])
 {
     using namespace ads_2;
     using parser::FileParser;
     using binary_tree::BinarySearchTree;
+    using hash_table::HashTable;
 
-    FileParser parser("dictionary-min.txt", 1000);
-    if (!parser.isValid()) return 1;
 
-    BinarySearchTree bst(&parser);
-    size_t start = time(NULL);
-    bst.compute();
-    size_t end = time(NULL);
-    std::cout << end - start << std::endl;
-    std::cout << bst.getNodeCount() << std::endl;
+    FileParser parser("dictionary.txt", static_cast<type::uintf>(1000));
+    FileParser sample("sample100000.txt", true);
+    if (!parser.isValid() || !sample.isValid()) return 1;
+//
+//    BinarySearchTree bst(&parser);
 
-    std::string word;
-    while (std::cin >> word)
-    {
-        if (word == "abc") break;
-        std::cout << word << ": " << bst.search(word) << std::endl;
-    }
+//    bst.compute();
+    HashTable ht(&parser);
+    ht.sortLists();
+
+    type::uintf hit_counter = 0;
+    size_t start = clock();
+    for (const auto &sample : sample.getSampleData())
+        hit_counter = hit_counter + ht.search(sample);
+
+    size_t end = clock();
+    std::cout << (end - start)/double(CLOCKS_PER_SEC)*1000 << std::endl;
+    std::cout << hit_counter << std::endl;
+
+
+
+
+
+//    std::string word;
+//    while (std::cin >> word)
+//    {
+//        if (word == "abc") break;
+//        std::cout << "ht.search(" << word << "): " << ht.search(word) << std::endl;
+//
+//    }
+
     return 0;
 }
