0ce1e089144a2977a40b1fce729da4f98b07b53d
Martin Kiesel <martin.kiesel@gmail.com>
2016-04-15 01:08:49 +0200

    hash tables complete

---
+++ ads-assignment-3/src/hash_table/hash_table.cpp
@@ -0,0 +1,64 @@
+//
+//  hash_table.cpp
+//  ads-assignment-3
+//
+//  Created by Martin Kiesel on 14/04/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#include "hash_table.hpp"
+
+#include "hash.cpp"
+
+namespace ads_2
+{
+    namespace hash_table
+    {
+        void HashTable::insert(const std::string &key)
+        {
+            type::uintf hash = generateHash(key) % table_size_;
+            table_[hash]->push_back(key);
+        }
+
+        bool HashTable::search(const std::string &key) const
+        {
+            type::uintf hash = generateHash(key) % table_size_;
+            type::uintf list_size = static_cast<type::uintf>(table_[hash]->size());
+
+            switch (list_size)
+            {
+                case 0:
+                    return false;
+                case 1:
+                    return table_[hash]->front() == key;
+                case 2:
+                    return (table_[hash]->front() == key || table_[hash]->back() == key);
+                default:
+                    auto end_it = table_[hash]->end();
+                    return std::find(table_[hash]->begin(), end_it, key) != end_it;
+                    break;
+            }
+        }
+
+        void HashTable::sortLists()
+        {
+            for (auto &list : table_)
+            {
+                if (list->empty() || list->size() == 1) continue;
+                list->sort();
+            }
+        }
+
+        void HashTable::printLists()
+        {
+            for (const auto &list : table_)
+            {
+                for (const auto &item : *list)
+                {
+                    std::cout << item << "\t";
+                }
+                std::cout << std::endl;
+            }
+        }
+    }
+}
---
+++ ads-assignment-3/src/hash_table/hash_table.hpp
@@ -0,0 +1,55 @@
+//
+//  hash_table.hpp
+//  ads-assignment-3
+//
+//  Created by Martin Kiesel on 14/04/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#ifndef hash_table_hpp
+#define hash_table_hpp
+
+#include "file_parser.hpp"
+
+namespace ads_2
+{
+    namespace hash_table
+    {
+        class HashTable
+        {
+            parser::FileParser *parser_;
+            type::KegDimension table_;
+            const type::uintf table_size_;
+            
+            void insert(const std::string &key);
+
+            HashTable& operator=(const HashTable&);
+            HashTable(const HashTable&);
+        public:
+            HashTable(parser::FileParser *parser) :
+                parser_(parser),
+                table_size_(parser_->getLength())
+            {
+                table_.resize(table_size_);
+
+                for (auto &item : table_)
+                    item = new type::KegList();
+
+                for (const auto &item : parser_->getData())
+                    insert(item.first);
+            }
+
+            void sortLists();
+            void printLists();
+            bool search(const std::string &key) const;
+
+            ~HashTable()
+            {
+                for (auto &item : table_)
+                    delete item;
+            }
+        };
+    }
+}
+
+#endif /* hash_table_hpp */
